4c39299f3271568baa4577b11aba87c40e7c3a11
Filip Maelbrancke <filip@maelbrancke.net>
2013-11-04 22:13:15 +0100

    Cleanup

--- TamperDetectionProject/TamperDetection/src/main/java/be/idamf/android/tamperdetection/MainActivity.java
+++ TamperDetectionProject/TamperDetection/src/main/java/be/idamf/android/tamperdetection/MainActivity.java
@@ -1,19 +1,17 @@
 package be.idamf.android.tamperdetection;
 
-import android.app.Activity;
 import android.app.ActionBar;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
 import java.util.Locale;
 
@@ -70,19 +68,18 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         switch (item.getItemId()) {
-            case R.id.action_settings:
+            case R.id.action_info:
+                new AlertDialog.Builder(this)
+                        .setTitle(R.string.about)
+                        .setMessage(Html.fromHtml(getString(R.string.msg_about)))
+                        .show();
                 return true;
         }
         return super.onOptionsItemSelected(item);
